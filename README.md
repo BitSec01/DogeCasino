687cd45d0fe635492c1791bf1ad7770e8769f146
Ramon <ramon.robben@live.nl>
2019-09-24 00:22:44 +0200

    Added Config
    added config to not make the secrets /

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-DogeCasino
+# DogeCasino
+
+DodgeCoin Casino a provable fair gambling website. With our provable fair system you can verify the outcome of all games.
--- index.js
+++ index.js
@@ -1,37 +1,10 @@
-const request = require('request-promise');
 const util = require("util");
+const express = require('express');
+const app = express();
 
-const api_key = 'c221-82b7-a728-d595';
-const secret = 'Kutkind01';
+const blockio = require('./module/blockio');
 
-const api_base_url = 'https://block.io/api/v2/';
-
-
-function get_total_balance(callback) {
-  request.get(`${api_base_url}get_balance/?api_key=${api_key}`).then((data) => {
-    callback(JSON.parse(data));
-  }, (error) => {
-    callback(JSON.parse(error.error));
-  });
-}
-
-function get_addresses(callback) {
-  request.get(`${api_base_url}get_my_addresses/?api_key=${api_key}`).then((data) => {
-    callback(JSON.parse(data));
-  }, (error) => {
-    callback(JSON.parse(error.error));
-  });
-}
-
-function create_address(label, callback) {
-  request.get(`${api_base_url}get_new_address/?api_key=${api_key}&label=${label}`).then((data) => {
-    callback(JSON.parse(data));
-  }, (error) => {
-    callback(JSON.parse(error.error));
-  });
-}
-
-create_address("test24", (out) => {
+blockio.create_address("test24", (out) => {
   if (out.status == 'fail') {
     console.log(out.data.error_message);
     return;
